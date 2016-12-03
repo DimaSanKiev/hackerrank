379255e623ef82c884e4f68c4732ec20bab4664b
Dmytro Burdyga <dmytro.burdyga@gmail.com>
2016-12-03 22:13:43 +0200

    Staircase

---
+++ src/algorithms/warmup/staircase/Solution.java
@@ -0,0 +1,21 @@
+package algorithms.warmup.staircase;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = n - i; k <= n; k++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
